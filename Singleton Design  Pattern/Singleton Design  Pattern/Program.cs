85784874808c80b972a1a1709c6785ddbfbe9ff2
Furkan Özkan <furkan.105398@gmail.com>
2023-05-01 14:46:21 +0300

    Creational Desing Pattern - Singleton Design Pattern

---
+++ Singleton Design  Pattern/Singleton Design  Pattern/Program.cs
@@ -0,0 +1,49 @@
+﻿
+DbConnection dbConnection = DbConnection.Connection;
+dbConnection.OpenConnection();
+dbConnection.CloseConnection();
+
+
+DbConnection dbConnection2 = DbConnection.Connection;
+dbConnection.OpenConnection();
+dbConnection.CloseConnection();
+
+
+DbConnection dbConnection3 = DbConnection.Connection;
+dbConnection.OpenConnection();
+dbConnection.CloseConnection();
+
+class DbConnection
+{
+	private DbConnection()
+	{
+		Console.WriteLine($"{nameof(DbConnection)}s class connection is being created ");
+	}
+
+	private static DbConnection _connection;
+
+	public static DbConnection Connection
+	{
+		get
+		{
+			if (_connection == null)
+			{
+				_connection = new DbConnection();
+			}
+			return _connection;
+		}
+	}
+
+	public void OpenConnection()
+	{
+		Console.WriteLine("Connection Open... ");
+		Console.WriteLine();
+	}
+
+	public void CloseConnection()
+	{
+		Console.WriteLine("Connection Close...");
+		Console.WriteLine();
+	}
+}
+
